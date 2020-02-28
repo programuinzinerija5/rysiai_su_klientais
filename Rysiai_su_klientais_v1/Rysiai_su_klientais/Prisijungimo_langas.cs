3d184dd97c03e91010bccd4723601f55a6b37996
programuinzinerija5 <60981425+programuinzinerija5@users.noreply.github.com>
2020-02-28 23:10:47 +0200

    Pirmas sprintas_GUI
    Sukurta grafinė sąsaja, pagrindiniame lange pridėti du mygtukai, vienas prisijungimui, kitas registracijai. Taip pat sukurti prisijungimo ir registracijos langai, paspaudus mygtuką "Prisijungimas" nukreips į prisijungimo langą, kuriame yra langeliai skirti login'ui ir slaptažodžiui įvesti, paspaudus "Registracija" nukreips į registracijos langą, kuriame yra langeliai informacijai pildyti.

---
+++ Rysiai_su_klientais_v1/Rysiai_su_klientais/Prisijungimo_langas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rysiai_su_klientais
+{
+    public partial class Prisijungimo_langas : Form
+    {
+        public Prisijungimo_langas()
+        {
+            InitializeComponent();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            this.Hide();
+            pradinis_langas p1 = new pradinis_langas();
+            p1.ShowDialog();
+            this.Close();
+        }
+    }
+}
